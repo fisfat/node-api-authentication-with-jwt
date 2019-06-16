421751858e175cda4a4e1a6519b4935e15ef6f39
fisfat <fatoyinbooluwafisayo@gmail.com>
2019-06-16 22:28:20 +0100

    tests

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -1,9 +1,39 @@
 const axios = require("axios");
+const User = require("../models/User");
 
 describe("#getUser() using async/await", () => {
-  it("should load user data", async () => {
+  test("should load user data", async () => {
     const data = await axios.get("http://localhost:5000/api/deliveries");
     expect(data.data).toBeDefined();
     expect(data.data.message).toEqual("done");
+  });
+});
+
+describe("Every possible occurence on a user model", () => {
+  it("insert a user", async () => {
+    const new_user = {
+      name: "Fisayo",
+      email: "kelly@kellysss.com",
+      password: "1",
+      password2: "1",
+      role: "user",
+      phone_number: "111111"
+    };
+    const response = await axios.post(
+      "http://localhost:5000/api/users/",
+      new_user
+    );
+    expect(response.data.name).toBe(new_user.name);
+    expect(response.data).toBe(typeof new_user);
+    expect(response.data).toBeTruthy();
+  });
+
+  it("should return a 400", async () => {
+    const new_user = {};
+    const response = await axios.post(
+      "http://localhost:5000/api/users/",
+      new_user
+    );
+    expect(response.status).toBeFalsy();
   });
 });
